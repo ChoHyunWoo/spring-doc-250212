1ea1a913621a28a53e3f74e54f26d689c555f364
ChoHyunWoo <gusdndlek12@naver.com>
2025-02-12 12:17:48 +0900

    ApiV1PostController design

--- backend/src/main/java/com/example/spring_doc/domain/member/member/controller/ApiV1MemberController.java
+++ backend/src/main/java/com/example/spring_doc/domain/member/member/controller/ApiV1MemberController.java
@@ -7,12 +7,15 @@ import com.example.spring_doc.domain.post.post.service.PostService;
 import com.example.spring_doc.global.Rq;
 import com.example.spring_doc.global.dto.RsData;
 import com.example.spring_doc.global.exception.ServiceException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "ApiV1MemberController", description = "회원 관련 API")
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
@@ -22,10 +25,10 @@ public class ApiV1MemberController {
     private final Rq rq;
     private final PostService postService;
 
-    record JoinReqBody(@NotBlank String username, @NotBlank String password, @NotBlank String nickname) {
-    }
+    record JoinReqBody(@NotBlank String username, @NotBlank String password, @NotBlank String nickname) {}
 
-    @PostMapping("/join")
+    @Operation(summary = "회원 가입")
+    @PostMapping(value = "/join", produces = "application/json;charset=UTF-8")
     public RsData<MemberDto> join(@RequestBody @Valid JoinReqBody reqBody) {
 
         memberService.findByUsername(reqBody.username())
@@ -43,12 +46,11 @@ public class ApiV1MemberController {
     }
 
 
-    record LoginReqBody(@NotBlank String username, @NotBlank String password) {
-    }
+    record LoginReqBody(@NotBlank String username, @NotBlank String password) {}
 
-    record LoginResBody(MemberDto item, String apiKey, String accessToken) {
-    }
+    record LoginResBody(MemberDto item, String apiKey, String accessToken) {}
 
+    @Operation(summary = "로그인", description = "로그인 성공 시 ApiKey와 AccessToken 반환. 쿠키로도 반환")
     @PostMapping("/login")
     public RsData<LoginResBody> login(@RequestBody @Valid LoginReqBody reqBody, HttpServletResponse response) {
 
@@ -56,7 +58,7 @@ public class ApiV1MemberController {
                 () -> new ServiceException("401-1", "잘못된 아이디입니다.")
         );
 
-        if (!member.getPassword().equals(reqBody.password())) {
+        if(!member.getPassword().equals(reqBody.password())) {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
 
@@ -76,16 +78,17 @@ public class ApiV1MemberController {
         );
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃 시 쿠키 삭제")
     @DeleteMapping("/logout")
     public RsData<Void> logout() {
 
-        rq.removeCokie("accessToken");
-        rq.removeCookie("apikey");
+        rq.removeCookie("accessToken");
+        rq.removeCookie("apiKey");
 
         return new RsData<>("200-1", "로그아웃 되었습니다.");
     }
 
-
+    @Operation(summary = "내 정보 조회")
     @GetMapping("/me")
     public RsData<MemberDto> me() {
 
